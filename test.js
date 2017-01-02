9e5086302ea890b553342d3cd89595e5dbb3224c
Nikhil Srivastava <niksrc@gmail.com>
2017-01-02 23:03:30 +0530

    add tests

--- test.js
+++ test.js
@@ -1,11 +1,29 @@
 import test from 'ava';
-import always from './';
+import run from './';
 
-test('title', t => {
+function shouldRun(t) {
+	let y = false;
 	return Promise
 		.resolve(1)
-		.then(always(x => x + 1))
+		.then(run(x => {
+			y = true;
+			x = 2;
+			return x;
+		}))
 		.then(x => {
-			t.is(x, 1)
+			t.is(x, 1);
+			t.is(y, true);
 		});
-});
+}
+
+async function shouldFail(t) {
+	try {
+		await run();
+		t.fail();
+	} catch (err) {
+		t.pass(err);
+	}
+}
+
+test('should run function', shouldRun);
+test('should fail with no param', shouldFail);
